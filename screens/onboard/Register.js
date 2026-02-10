125ef1b060ac96265c3620ae45edfb4b13ffc7a9
Young Money <76821570+Youngmoney40@users.noreply.github.com>
2026-02-10 15:58:39 +0100

    Adjust header padding for non-iOS platforms

--- screens/onboard/Register.js
+++ screens/onboard/Register.js
@@ -665,7 +665,7 @@ const styles = StyleSheet.create({
     flexGrow: 1
   },
   header: {
-    paddingTop: Platform.OS === 'ios' ? 0 : 30,
+    paddingTop: Platform.OS === 'ios' ? 0 : 20,
     paddingBottom: 30,
     paddingHorizontal: 0,
     borderBottomLeftRadius: 0,
